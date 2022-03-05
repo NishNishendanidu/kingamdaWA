d29137443b63576774d32953e600601df4c333f2
Shen bro <96036613+NishNishendanidu@users.noreply.github.com>
2022-03-05 14:17:40 +0530

    Update and rename TrexTeam.js to kingamda-XTeam.js

--- kingamda-XTeam.js
+++ kingamda-XTeam.js
@@ -1,5 +1,5 @@
 module.exports = {
-    ALPHA_OWNER: '', // Trex Team - Alpha Owner"94772830536"
+    KINGAMDA_OWNER: '', // Kingamda-X Team - Kingamda-X Owner"94772830536"
     TREX_TEAM: '94772830536,94772830536,94772830536,94772830536,94772830536', // Team - Kingamda-X
     TREX_OWNER: '94772830536' // Nishen Danidu-co Owner in kingamda-X
 };
