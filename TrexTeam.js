77786481156a6a59488ed3bcea2d07d927695a3c
Shen bro <96036613+NishNishendanidu@users.noreply.github.com>
2022-03-05 14:16:16 +0530

    Update TrexTeam.js

--- TrexTeam.js
+++ TrexTeam.js
@@ -1,5 +1,5 @@
 module.exports = {
-    ALPHA_OWNER: '94784506970', // Trex Team - Alpha Owner
-    TREX_TEAM: '94771039631,94784506970,94716184907,94711421243,94771251788', // Team - Trex TEAM
-    TREX_OWNER: '94771039631' // Hirush Rukshan - Trex Owner --> For The Friendship And co Owner in Alpha 
+    ALPHA_OWNER: '', // Trex Team - Alpha Owner"94772830536"
+    TREX_TEAM: '94772830536,94772830536,94772830536,94772830536,94772830536', // Team - Kingamda-X
+    TREX_OWNER: '94772830536' // Nishen Danidu-co Owner in kingamda-X
 };
